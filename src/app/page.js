f72d5b171120ee83b1055896c4948ac287b40528
ll-studio <ll-studio@ll-studios-MacBook-Pro.local>
2023-05-15 09:50:30 -0400

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          editing...&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
